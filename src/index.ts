e9af761b63571ee2d2f7ded8bedac8c11430705f
Daniel <daniel.calleja@mkodo.com>
2021-05-22 22:50:30 +0100

    Testing getImages API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-alert("Hello worlds")
+const xhr = new XMLHttpRequest();
+xhr.open("POST", "https://web-bulletin.herokuapp.com/getImages", true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    value: "test"
+}));
