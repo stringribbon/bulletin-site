419bedbce9217672657c14be26116fb6d38b027d
Daniel <pigrsi.boat@gmail.com>
2021-05-23 13:45:24 +0100

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-typescript"],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
     ],
   },
   resolve: {
