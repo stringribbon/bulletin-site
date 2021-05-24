d13c8298516e03235b1d753c11c924d8889ff0fb
Daniel <pigrsi.boat@gmail.com>
2021-05-24 18:58:19 +0100

    Use proper giphy types

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,9 +1,11 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
+import { IGif } from '@giphy/js-types';
 import GiphyFetchMock from "./mocks/giphy-fetch-mock";
 import { giphyApiKey, useMockApis } from "../../config";
 
 const giphyFetch = useMockApis ? GiphyFetchMock : new GiphyFetch(giphyApiKey);
 
-export function fetchGiphySearch(term: string) {
-    return giphyFetch.search(term, { offset: 0, limit: 10 });
+export async function fetchGiphySearch(term: string, offset: number, limit: number): Promise<IGif[]> {
+    const response = await giphyFetch.search(term, { offset, limit });
+    return (response.data as IGif[]);
 }
--- src/gif-picker/gif-picker.ts
+++ src/gif-picker/gif-picker.ts
@@ -2,10 +2,15 @@ import $ from 'jquery';
 import pickerHtml from './gif-picker.html';
 import "./gif-picker.css";
 import { fetchGiphySearch } from '../api/requests';
+import { IGif } from '@giphy/js-types';
 
 export async function showGifPicker() {
     $(pickerHtml).appendTo('body');
 
-    const result = await fetchGiphySearch("rat");
-    console.log(result.data);
+    const searchResult = await fetchGiphySearch("rat", 0, 10);
+    showSelection(searchResult);
+}
+
+function showSelection(gifSelection: IGif[]) {
+    console.log(gifSelection);
 }
