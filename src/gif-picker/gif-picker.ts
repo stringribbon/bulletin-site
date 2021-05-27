126add34049d3c55146b196b8f34208d197883fa
Daniel <pigrsi.boat@gmail.com>
2021-05-27 21:41:50 +0100

    Autosearch more gifs when div is scrolled to bottom

--- src/gif-picker/gif-picker.ts
+++ src/gif-picker/gif-picker.ts
@@ -7,6 +7,7 @@ import { INetwork } from '../types/INetwork';
 type Callback = (selectedUrl: string) => any;
 let selectedCallback: Callback;
 let network: INetwork;
+let searchOffset: number = 0;
 
 export async function showGifPicker(networkObj: INetwork, callback: Callback) {
     network = networkObj;
@@ -15,10 +16,15 @@ export async function showGifPicker(networkObj: INetwork, callback: Callback) {
     if ($('#gif-picker').length === 0) {
         $(pickerHtml).appendTo('#board');
         $('#search-input').on( "keydown", event => {
-            if (event.key === "Enter") doSearch();
+            if (event.key === "Enter") doSearch(true);
         });
         $('#search-button').on("click", () => {
-            doSearch();
+            doSearch(true);
+        });
+        $('#gif-grid').on('scroll', function() {
+            if(searchOffset > 0 && $(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+                doSearch(false);
+            }
         });
     } else {
         $('#gif-picker').show();
@@ -28,20 +34,30 @@ export async function showGifPicker(networkObj: INetwork, callback: Callback) {
     $('#board-overlay').on('click', () => hide(true));
 }
 
-async function doSearch() {
+async function doSearch(firstSearch: boolean) {
     const term = ($('#search-input').val() as string).trim();
     if (!term) return;
 
+    if (firstSearch) {
+        searchOffset = 0;
+    }
+    if (searchOffset > 200) return;
+
+    const numToSearch = 16;
     $('#search-button-text').hide();
     $('#search-button-spinner').show();
-    const searchResult = await network.fetchGiphySearch(term, 0, 10);
+    $('#search-button').attr('disabled', "true");
+    const searchResult = await network.fetchGiphySearch(term, searchOffset, numToSearch);
     $('#search-button-text').show();
     $('#search-button-spinner').hide();
-    showSelection(searchResult);
+    $('#search-button').removeAttr('disabled');
+    showSelection(searchResult, searchOffset === 0);
+    searchOffset += numToSearch;
 }
 
-function showSelection(gifSelection: IGif[]) {
-    const grid = $('#gif-grid').empty();
+function showSelection(gifSelection: IGif[], emptyFirst: boolean) {
+    const grid = $('#gif-grid');
+    if (emptyFirst) grid.empty();
 
     gifSelection.forEach(gif => {
         const url = gif.images.downsized_large.url.split('?')[0];
